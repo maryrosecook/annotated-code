b7b65cad2f0b269ae98443e2fb79d92dec58d10c
Mary Rose Cook <maryrosecook@maryrosecook.com>
2014-07-27 00:03:19 -0400

    rm unneeded parens in arithmetic

--- circles-bouncing-off-lines/circles-bouncing-off-lines.js
+++ circles-bouncing-off-lines/circles-bouncing-off-lines.js
@@ -119,7 +119,7 @@
     distance: function(point1, point2) {
       var x = point1.x - point2.x;
       var y = point1.y - point2.y;
-      return Math.sqrt((x * x) + (y * y));
+      return Math.sqrt(x * x + y * y);
     },
 
     magnitude: function(vector) {
